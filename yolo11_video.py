889149ff074b885c95d64313352af14feae14451
contrivedexample <bob@crowley.software>
2025-02-04 21:27:56 -0500

    move detection result to file

--- yolo11_video.py
+++ yolo11_video.py
@@ -11,28 +11,7 @@ from scipy.spatial import KDTree
 from webcolors import (
     hex_to_rgb,
 )
-
-class DetectionResult:
-    def __init__(self, cls, confidence, result_number, color, aspect_ratio, class_name):
-        self.cls = cls
-        self.confidence = confidence
-        self.result_number = result_number
-        self.color = color
-        self.aspect_ratio = aspect_ratio
-        self.class_name = class_name
-
-    def __repr__(self):
-        return f"DetectionResult(class={self.cls}, confidence={self.confidence}, result_number={self.result_number}, color={self.color}, class_name={self.class_name})"
-
-    def to_dict(self):
-        return {
-            "class": self.cls,
-            "class_name": self.class_name,
-            "confidence": self.confidence,
-            "result_number": self.result_number,
-            "color": self.color,
-            "aspect_ratio": self.aspect_ratio
-        }
+from detection_result import DetectionResult  # Import the DetectionResult class
         
 def convert_rgb_to_names(rgb_tuple):
     
